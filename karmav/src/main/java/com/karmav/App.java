ab4a466698c1bbb11eb11a167e91648846712cc0
root <root@ip-172-31-17-79.ap-southeast-1.compute.internal>
2021-08-21 09:25:15 +0000

    Initial Commit

---
+++ karmav/src/main/java/com/karmav/App.java
@@ -0,0 +1,13 @@
+package com.karmav;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
